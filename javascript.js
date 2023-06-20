30c45ffea6ce4608474c62d6b531c9c66115d6ba
sheezu900 <shahzamanlive2@gmail.com>
2023-06-20 15:00:33 +0500

    resolve linters error

--- javascript.js
+++ javascript.js
@@ -1,26 +1,26 @@
-const menuIcon = document.getElementById("menu");
-const menuItems = document.querySelectorAll("li > a");
+const menuIcon = document.getElementById('menu');
+const menuItems = document.querySelectorAll('li > a');
 
-document.getElementById("icon").onclick = () => {
+document.getElementById('icon').onclick = () => {
 	menuIcon.style.display =
-		menuIcon.style.display === "flex" ? "none" : "flex";
+		menuIcon.style.display === 'flex' ? 'none' : 'flex';
 };
 
-document.getElementById("crossIcon").onclick = () => {
+document.getElementById('crossIcon').onclick = () => {
 	menuIcon.style.display =
-		menuIcon.style.display === "flex" ? "none" : "flex";
+		menuIcon.style.display === 'flex' ? 'none' : 'flex';
 };
 
 menuItems.forEach((individualSection) => {
 	individualSection.onclick = () => {
 		menuIcon.style.display =
-			menuIcon.style.display === "flex" ? "none" : "flex";
+			menuIcon.style.display === 'flex' ? 'none' : 'flex';
 	};
 });
 
 function scrollSection(event) {
 	event.preventDefault();
-	const sectionId = this.getAttribute("href");
+	const sectionId = this.getAttribute('href');
 	const targetedSection = document.querySelector(sectionId);
-	targetedSection.scrollIntoView({ behavior: "smooth" });
+	targetedSection.scrollIntoView({ behavior: 'smooth' });
 }

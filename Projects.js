c9abfd4e11304cf093a8fbfc4eeba0f5a0cd7f38
sheezu900 <shahzamanlive2@gmail.com>
2023-06-21 19:53:06 +0500

    resolve eslint error

--- Projects.js
+++ Projects.js
@@ -1,77 +1,76 @@
-let projects = [
-  {
-    id: 1,
-    card: 'card1',
-    image: './images/SnapshootPortfolio.svg',
-    Projectname: 'Tonic',
-    history: {
-      proName: 'CANOPY',
-      tech: 'Backend Dev',
-      date: 2015,
-    },
-    disc: 'A daily selection of privately personalized reads;no accounts or sign-ups required.',
-    tags: {
-      html: 'HTML',
-      css: 'CSS',
-      javascript: 'JavaScript'
-    },
-    button: 'myBtn'
+const projects = [{
+  id: 1,
+  card: 'card1',
+  image: './images/SnapshootPortfolio.svg',
+  Projectname: 'Tonic',
+  history: {
+    proName: 'CANOPY',
+    tech: 'Backend Dev',
+    date: 2015,
   },
-  {
-    id: 2,
-    card: 'card2',
-    image: './images/multiPostStories.svg',
-    Projectname: 'Multi-Post',
-    history: {
-      proName: 'CANOPY',
-      tech: 'Backend Dev',
-      date: 2015,
-    },
-    disc: 'A daily selection of privately personalized reads;no accounts or sign-ups required.',
-    tags: {
-      html: 'HTML',
-      css: 'CSS',
-      javascript: 'JavaScript'
-    },
-    button: 'myBtn'
+  disc: 'A daily selection of privately personalized reads;no accounts or sign-ups required.',
+  tags: {
+    html: 'HTML',
+    css: 'CSS',
+    javascript: 'JavaScript',
   },
-  {
-    id: 3,
-    card: 'card3',
-    image: './images/SnapshootPortfolio.svg',
-    Projectname: 'Tonic',
-    history: {
-      proName: 'CANOPY',
-      tech: 'Backend Dev',
-      date: 2015,
-    },
-    disc: 'A daily selection of privately personalized reads;no accounts or sign-ups required.',
-    tags: {
-      html: 'HTML',
-      css: 'CSS',
-      javascript: 'JavaScript'
-    },
-    button: 'myBtn'
+  button: 'myBtn',
+},
+{
+  id: 2,
+  card: 'card2',
+  image: './images/multiPostStories.svg',
+  Projectname: 'Multi-Post',
+  history: {
+    proName: 'CANOPY',
+    tech: 'Backend Dev',
+    date: 2015,
   },
-  {
-    id: 4,
-    card: 'card4',
-    image: './images/multiPostStories.svg',
-    Projectname: 'MultiPost',
-    history: {
-      proName: 'CANOPY',
-      tech: 'Backend Dev',
-      date: 2015,
-    },
-    disc: 'A daily selection of privately personalized reads;no accounts or sign-ups required.',
-    tags: {
-      html: 'HTML',
-      css: 'CSS',
-      javascript: 'JavaScript'
-    },
-    button: 'myBtn'
-  }
-]
+  disc: 'A daily selection of privately personalized reads;no accounts or sign-ups required.',
+  tags: {
+    html: 'HTML',
+    css: 'CSS',
+    javascript: 'JavaScript',
+  },
+  button: 'myBtn',
+},
+{
+  id: 3,
+  card: 'card3',
+  image: './images/SnapshootPortfolio.svg',
+  Projectname: 'Tonic',
+  history: {
+    proName: 'CANOPY',
+    tech: 'Backend Dev',
+    date: 2015,
+  },
+  disc: 'A daily selection of privately personalized reads;no accounts or sign-ups required.',
+  tags: {
+    html: 'HTML',
+    css: 'CSS',
+    javascript: 'JavaScript',
+  },
+  button: 'myBtn',
+},
+{
+  id: 4,
+  card: 'card4',
+  image: './images/multiPostStories.svg',
+  Projectname: 'MultiPost',
+  history: {
+    proName: 'CANOPY',
+    tech: 'Backend Dev',
+    date: 2015,
+  },
+  disc: 'A daily selection of privately personalized reads;no accounts or sign-ups required.',
+  tags: {
+    html: 'HTML',
+    css: 'CSS',
+    javascript: 'JavaScript',
+  },
+  button: 'myBtn',
+},
+];
 
 const section = document.getElementById('works');
 
@@ -80,141 +79,130 @@ const projectMethod = () => {
     const project = document.createElement('div');
     project.classList.add('card_works');
     project.classList.add(card.card);
-    project.innerHTML= `<img
-					class="card_image "
-					src=${card.image}
-					alt="Tonic Project"
-				/>
-				<div class="card_detail">
-					<div class="card_title">
-						<h2>${card.Projectname}</h2>
-						<div class="card_history">
-							<div class="card_history_detail"><p>${card.history['proName']}</p></div>
-							<div>
-								<img src="./images/Counter.svg" alt="dot" />
-							</div>
-							<div class="card_history_detail">
-								<p>${card.history['tech']}</p>
-							</div>
-							<div>
-								<img src="./images/Counter.svg" alt="dot" />
-							</div>
-							<div class="card_history_detail"><p>${card.history['date']}</p></div>
-						</div>
-					</div>
-					<div class="card_discription">
-						<p>${card.disc}</p>
-					</div>
-					<div class="card_tags">
-						<div class="tag">
-							<span class="small">${card.tags['html']}</span>
-						</div>
-						<div class="tag">
-							<span class="small">${card.tags['css']}</span>
-						</div>
-						<div class="tag">
-							<span class="small">${card.tags['javascript']}</span>
-						</div>
-					</div>
-					<div class="card_action">
-						<button onclick="button(${card.id})" class="btn" id="${card.button}" type="button">See Project</button>
-					</div>
-				</div>`
+    project.innerHTML = `<img
+              class="card_image "
+              src=${card.image}
+              alt="Tonic Project"
+              />
+              <div class="card_detail">
+              <div class="card_title">
+              <h2>${card.Projectname}</h2>
+                <div class="card_history">
+                  <div class="card_history_detail"><p>${card.history.proName}</p></div>
+                <div>
+                   <img src="./images/Counter.svg" alt="dot" />
+                </div>
+                <div class="card_history_detail">
+                <p>${card.history.tech}</p>
+                  </div>
+                  <div>
+                  <img src="./images/Counter.svg" alt="dot" />
+                   </div>
+                  <div class="card_history_detail"><p>${card.history.date}</p></div>
+                </div>
+              </div>
+              <div class="card_discription">
+              <p>${card.disc}</p>
+              </div>
+               <div class="card_tags">
+                <div class="tag">
+                 <span class="small">${card.tags.html}</span>
+                 </div>
+                  <div class="tag">
+                      <span class="small">${card.tags.css}</span>
+                 </div>
+                 <div class="tag">
+                         <span class="small">${card.tags.javascript}</span>
+                 </div>
+               </div>
+              <div class="card_action">
+              <button onclick="button(${card.id})" class="btn" id="${card.button}" type="button">See Project</button>
+              </div>
+               </div>`;
 
-    section.appendChild(project)
-  })
-}
+    return section.appendChild(project);
+  });
+};
 
 projectMethod();
 
+const modal = document.getElementById('model');
 
-
-var modal = document.getElementById("model");
-
-
+// eslint-disable-next-line no-unused-vars
 function button(id) {
-  modal.style.display = "flex";
+  modal.style.display = 'flex';
   const card = projects.find((card) => card.id === id);
   const project = document.createElement('div');
-    project.classList.add('modelsection');
+  project.classList.add('modelsection');
   project.classList.add(card.card);
-    project.innerHTML= `<div class="header">
-					<div>
-						<h2>${card.Projectname}</h2>
-						<div class="card_historyy">
-							<div class="card_history_detaill"><p>${card.history['proName']}</p></div>
-							<div>
-								<img src="./images/Counter.svg" alt="dot" />
-							</div>
-							<div class="card_history_detail">
-								<p>${card.history['tech']}</p>
-							</div>
-							<div>
-								<img src="./images/Counter.svg" alt="dot" />
-							</div>
-							<div class="card_history_detail"><p>${card.history['date']}</p></div>
-						</div>
-					</div>
-					<a href="#" id="modelcross" class="Menuicon"
-						><span
-							id="modelcrossIcon"
-							style="color: black"
-							class="material-symbols-outlined"
-						>
-							close
-						</span>
-					</a>
-				</div>
-				<div class="modelimage">
-					<img
-						class="card_imagee"
-						src=${card.image}
-						alt="Tonic Project"
-					/>
-				</div>
-				<div class="modeldisc">
-					<div class="card_discription">
-						<p>
-							${card.disc}
-						</p>
-					</div>
-					<div class="card_tags">
-						<div class="modeltags">
-							<div class="tag">
-								<span class="small">${card.tags['html']}</span>
-							</div>
-							<div class="tag">
-								<span class="small">${card.tags['css']}</span>
-							</div>
-							<div class="tag">
-								<span class="small"
-									>${card.tags['javascript']}</span
-								>
-							</div>
-						</div>
-						<div class="card_action">
-						<button class="modelbtn" type="button"> See lice <img style="height: 24px; width: 24px; margin-left: 6px;" src="./images/see_live.svg" alt=""></button>
-						<button class="modelbtn" type="button"> See source <img style="height: 24px; width: 24px; margin-left: 6px;" src="./images/see source.svg" alt=""> </button>
-					</div>
-					</div>
-				</div>`
+  project.innerHTML = `<div class="header">
+             <div>
+            <h2>${card.Projectname}</h2>
+             <div class="card_historyy">
+               <div class="card_history_detaill"><p>${card.history.proName}</p></div>
+              <div>
+                     <img src="./images/Counter.svg" alt="dot" />
+              </div>
+                 <div class="card_history_detail">
+                <p>${card.history.tech}</p>
+              </div>
+                <div>
+               <img src="./images/Counter.svg" alt="dot" />
+               </div>
+               <div class="card_history_detail"><p>${card.history.date}</p></div>
+             </div>
+             </div>
+             <a href="#" id="modelcross" class="Menuicon"
+            ><span
+                    id="modelcrossIcon"
+             style="color: black"
+                  class="material-symbols-outlined"
+              >
+                  close
+            </span>
+          </a>
+        </div>
+     <div class="modelimage">
+          <img
+             class="card_imagee"
+              src=${card.image}
+           alt="Tonic Project"
+           />
+           </div>
+           <div class="modeldisc">
+            <div class="card_discription">
+             <p>
+                 ${card.disc}
+               </p>
+          </div>
+            <div class="card_tags">
+             <div class="modeltags">
+          <div class="tag">
+                 <span class="small">${card.tags.html}</span>
+              </div>
+              <div class="tag">
+               <span class="small">${card.tags.css}</span>
+                 </div>
+                <div class="tag">
+              <span class="small"
+                 >${card.tags.javascript}</span
+                       >
+               </div>
+                 </div>
+             <div class="card_action">
+                  <button class="modelbtn" type="button"> See lice <img style="height: 24px; width: 24px; margin-left: 6px;" src="./images/see_live.svg" alt=""></button>
+                   <button class="modelbtn" type="button"> See source <img style="height: 24px; width: 24px; margin-left: 6px;" src="./images/see source.svg" alt=""> </button>
+         </div>
+          </div>
+       </div>`;
 
   modal.appendChild(project);
   setTimeout(() => {
-    span = document.getElementById("modelcrossIcon");
-    span.onclick = function() {
-      modal.style.display = "none";
-      modal.removeChild(project)
-      console.log('working');
-    }
+    const span = document.getElementById('modelcrossIcon');
+    // eslint-disable-next-line func-names
+    span.onclick = function () {
+      modal.style.display = 'none';
+      modal.removeChild(project);
+    };
   }, 50);
 }
-
-var span = document.getElementById("modelcrossIcon");
-
-
-
-// function closeModel(id) {
-//   modal.style.display = "none";
-
-// }
